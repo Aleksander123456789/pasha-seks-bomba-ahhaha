7511e8ddfabec4e8dd13a33948fa0b70ed5f9da3
Aleksander123456789 <sanchagood@yandex.ru>
2024-01-17 18:10:23 +0500

    cube

--- cube.py
+++ cube.py
@@ -4,30 +4,18 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-    )
+    (1, -1, -1), (1, 1, -1),
+    (-1, 1, -1), (-1, -1, -1),
+    (1, -1, 1), (1, 1, 1),
+    (-1, -1, 1), (-1, 1, 1)
+)
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+    (0, 1), (0, 3), (0, 4),
+    (2, 1), (2, 3), (2, 7),
+    (6, 3), (6, 4), (6, 7),
+    (5, 1), (5, 4), (5, 7)
+)
 
 def Cube():
     glBegin(GL_LINES)
@@ -36,20 +24,37 @@ def Cube():
             glVertex3fv(verticies[vertex])
     glEnd()
 
+def draw_points_on_cube(size=0.1):
+    glPointSize(5.0)
+    glBegin(GL_POINTS)
+    for vertex in verticies:
+        glVertex3fv(vertex)
+    glEnd()
+
+def draw_center_point():
+    glPointSize(10.0)
+    glBegin(GL_POINTS)
+    glVertex3f(0, 0, 0)  # Центр куба
+    glEnd()
+
 def main():
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0,0.0, -5)
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        glRotatef(1, 3, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         Cube()
+        draw_points_on_cube()
+        draw_center_point()  
         pygame.display.flip()
-        pygame.time.wait(10)
-main() 
+
+main()
+
