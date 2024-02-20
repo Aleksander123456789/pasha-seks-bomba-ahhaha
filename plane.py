84a4f3fdbd4add92a612eb41ea56166249f4470d
Aleksander123456789 <sanchagood@yandex.ru>
2024-02-20 19:33:09 +0500

    best commit

--- plane.py
+++ plane.py
@@ -1,21 +1,14 @@
-def test_a(a: int) -> bool:
-    for x in range(1000):
-        for y in range(1000):
-            if (48 != y + 2*x) or (a < x) or (a < y):
-                pass
-            else:
-                return False
-    return True
+#2.2.6
+str1 = input("Введите первую строку чисел через пробел: ")
+str2 = input("Введите вторую строку чисел через пробел: ")
+list1 = list(map(int, str1.split()))
+list2 = list(map(int, str2.split()))
+common_list = list1 + list2
 
-for a in range(300,0,-1):
-    try:
-        for x in range(1000):
-            for y in range(1000):
-                if (48 != y + 2*x) or (a < x) or (a < y):
-                    pass
-                else:
-                    raise ValueError("не подходит a")
-    except ValueError:
-        continue
-    print(a)
-    break            
+n = len(common_list)
+for i in range(n-1):
+    for j in range(n-1):
+        if common_list[j] > common_list[j+1]:
+            common_list[j], common_list[j+1] = common_list[j+1], common_list[j]
+
+print(common_list)
